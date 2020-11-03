03940c14cf9b27d281cb0affa81c68acc975888e
Delphine Ducros <ducros@tourduvalat.org>
2020-11-03 15:06:00 +0100

    :tropical-fish:

---
+++ R/data_wildfinder.R
@@ -0,0 +1,36 @@
+
+#' Get ecoregion data
+#'
+#' @return data.frame with info on ecoregions
+#' @export
+#'
+data_ecoregion <- function() {
+
+  readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-ecoregions_list.csv"))
+
+}
+
+
+
+#' Get mammal species data
+#'
+#' @return data.frame with info on mammal species
+#' @export
+#'
+data_mammals <- function() {
+
+  readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-mammals_list.csv"))
+
+}
+
+
+#' Get mammal <-> ecoregions data
+#'
+#' @return data.frame with info on which mammal species in ecoregions
+#' @export
+#'
+data_mammals_ecoregions <- function() {
+
+  readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-ecoregions_species.csv"))
+
+}
